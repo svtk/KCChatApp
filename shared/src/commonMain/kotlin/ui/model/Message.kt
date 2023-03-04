20569d5a2d0d5e89153de2c7ed5115cce91240f6
svetlana.isakova <svetlana.isakova@jetbrains.com>
2023-03-04 22:05:46 +0100

    Changed time format

--- shared/src/commonMain/kotlin/ui/model/Message.kt
+++ shared/src/commonMain/kotlin/ui/model/Message.kt
@@ -1,28 +1,36 @@
 package ui.model
 
 import androidx.compose.runtime.Immutable
-import model.MessageEvent
 import kotlinx.datetime.*
 import kotlinx.serialization.Serializable
+import model.MessageEvent
 
 @Immutable
 @Serializable
 data class Message(
     val username: String,
     val text: String,
-    val localDateTime: LocalDateTime
+    val timestamp: Instant,
 )
 
 val MessageEvent.message
     get() = Message(
         username = username,
         text = messageText,
-        localDateTime = timestamp.toLocalDateTime(TimeZone.currentSystemDefault())
+        timestamp = timestamp,
     )
 
 fun Message.timeText(): String {
+    val localDateTime = timestamp.toLocalDateTime(TimeZone.currentSystemDefault())
     val time = localDateTime.run { LocalTime(hour, minute) }
+
     val date = localDateTime.date
     val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
-    return if (date == today) "$time" else "$date $time"
+    if (date == today) return "$time"
+
+    val month = localDateTime.month.name.lowercase()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        .substring(0..2)
+    return "$month ${localDateTime.dayOfMonth}, $time"
+
 }
