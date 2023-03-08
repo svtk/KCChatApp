e34bd3df5855b1b922efd37e1363e97e93917157
anton <ant.arhipov@gmail.com>
2023-03-08 16:29:13 +0200

    gradient background for iOS

--- shared/src/iosArm64Main/kotlin/ui/ChatBackground.kt
+++ shared/src/iosArm64Main/kotlin/ui/ChatBackground.kt
@@ -1,7 +1,20 @@
 package ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 @Composable
 internal actual fun ChatBackground() {
+    val gradientColors = listOf(Color.Cyan, Color.White)
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(gradientColors)
+            )
+    )
 }
